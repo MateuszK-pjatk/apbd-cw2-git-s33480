a60cb6dcdb2ea9ef93aced2b96095dae5d478bec
Mateusz Kanclerski <s33480@pjwstk.edu.pl>
2026-03-25 18:54:28 +0100

    Add ReturnEquipment logic with penalty

--- Services/RentalService.cs
+++ Services/RentalService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Proj1.Models.Users;
+﻿using Proj1.Models.Users;
 using Proj1.Models.Equipments;
 using Proj1.Models.Rentals;
 
@@ -8,6 +7,8 @@ namespace Proj1.Services;
 public class RentalService
 {
     private List<Rental> _rentals = new List<Rental>();
+    
+    private const decimal DailyPenaltyRate = 5.0m;
 
     public void RentEquipment(User user, Equipment equipment, int days)
     {
@@ -38,5 +39,71 @@ public class RentalService
         Rental newRental = new Rental(user, equipment, days);
         equipment.IsAvailable = false;
         _rentals.Add(newRental);
+    }
+
+    public void ReturnEquipment(Equipment equipment)
+    {
+        Rental activeRental = null;
+
+        foreach (var rental in _rentals)
+        {
+            if (rental.RentedItem.Id == equipment.Id && rental.ReturnDate == null)
+            {
+                activeRental = rental;
+                break;
+            }
+        }
+
+        if (activeRental == null)
+        {
+            throw new Exception($"Sprzet {equipment.Name} nie jest obecnie wypozyczony");
+        }
+        
+        activeRental.ReturnDate = DateTime.Now;
+
+        if (activeRental.ReturnDate > activeRental.DueDate)
+        {
+            TimeSpan delay = activeRental.ReturnDate.Value - activeRental.DueDate;
+            
+            int daysLate = (int)Math.Ceiling(delay.TotalDays);
+
+            if (daysLate > 0)
+            {
+                activeRental.PenaltyFee = daysLate * DailyPenaltyRate;
+            }
+        }
+        
+        equipment.IsAvailable = true;
+    }
+
+    public List<Rental> GetAllRentals()
+    {
+        return _rentals;
+    }
+
+    public List<Rental> GetActiveRentalsForUser(User user)
+    {
+        List<Rental> userRentals = new List<Rental>();
+        foreach (var rental in _rentals)
+        {
+            if (rental.RentedBy.Id == user.Id && rental.ReturnDate == null)
+            {
+                userRentals.Add(rental);
+            }
+        }
+        return userRentals;
+    }
+    
+    public List<Rental> GetOverdueRentals()
+    {
+        List<Rental> overdueRentals = new List<Rental>();
+        foreach (var rental in _rentals)
+        {
+            if (rental.ReturnDate == null && DateTime.Now > rental.DueDate)
+            {
+                overdueRentals.Add(rental);
+            }
+        }
+        return overdueRentals;
     }
 }
