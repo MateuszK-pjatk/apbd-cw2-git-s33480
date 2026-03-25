a474cca6de6722f91bf999d4b7c6e66b01f17268
Mateusz Kanclerski <s33480@pjwstk.edu.pl>
2026-03-25 16:09:00 +0100

    Add Equipment domain models

---
+++ Models/Equipment/Laptop.cs
@@ -0,0 +1,13 @@
+﻿namespace Proj1.Models.Equipment;
+
+public class Laptop : Equipment
+{
+    public string Processor { get; set; }
+    public int RamSizeGB { get; set; }
+
+    public Laptop(string name, string processor, int ramSizeGB) : base(name)
+    {
+        Processor = processor;
+        RamSizeGB = ramSizeGB;
+    }
+}
